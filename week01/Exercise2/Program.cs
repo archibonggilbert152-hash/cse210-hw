3b9a079eb781acfad0c4e5bd07af264b42b484f5
archibonggilbert152-hash <archibonggilbert152@gmail.com>
2025-10-30 16:50:02 -0700

    changes to program2

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,68 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        // Ask the user for their grade percentage
+        Console.Write("Enter your grade percentage: ");
+        string userInput = Console.ReadLine();
+        int gradePercentage = int.Parse(userInput);
+
+        string letter;
+        string sign = "";
+
+        // Determine the letter grade
+        if (gradePercentage >= 90)
+        {
+            letter = "A";
+        }
+        else if (gradePercentage >= 80)
+        {
+            letter = "B";
+        }
+        else if (gradePercentage >= 70)
+        {
+            letter = "C";
+        }
+        else if (gradePercentage >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        // Determine the sign (+ or -)
+        int lastDigit = gradePercentage % 10;
+
+        if (letter != "F" && letter != "A") // F and A have special rules
+        {
+            if (lastDigit >= 7)
+            {
+                sign = "+";
+            }
+            else if (lastDigit < 3)
+            {
+                sign = "-";
+            }
+        }
+        else if (letter == "A" && gradePercentage < 93)
+        {
+            sign = "-"; // A- only
+        }
+
+        // Combine letter and sign
+        Console.WriteLine($"Your grade is: {letter}{sign}");
+
+        // Pass/fail message
+        if (gradePercentage >= 70)
+        {
+            Console.WriteLine("Congratulations! You passed the class!");
+        }
+        else
+        {
+            Console.WriteLine("Keep trying! You can do better next time!");
+        }
     }
 }
+
+
