a85535f7672a66868503c79b120c113ba196da23
archibonggilbert152-hash <archibonggilbert152@gmail.com>
2025-10-30 17:09:47 -0700

    changes to program 3

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,54 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        string playAgain = "yes";
+
+        while (playAgain == "yes")
+        {
+            // Generate a random magic number between 1 and 100
+            Random randomGenerator = new Random();
+            int magicNumber = randomGenerator.Next(1, 101);
+
+            int guess = -1;
+            int guessCount = 0;
+
+            Console.WriteLine("I'm thinking of a number between 1 and 100...");
+
+            while (guess != magicNumber)
+            {
+                Console.Write("What is your guess? ");
+                string userInput = Console.ReadLine();
+
+                // Validate input
+                bool isValid = int.TryParse(userInput, out guess);
+                if (!isValid)
+                {
+                    Console.WriteLine("Please enter a valid number!");
+                    continue; // skip the rest of the loop and ask again
+                }
+
+                guessCount++;
+
+                if (guess < magicNumber)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else if (guess > magicNumber)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else
+                {
+                    Console.WriteLine($"🎉 You guessed it in {guessCount} tries!");
+                }
+            }
+
+            // Ask if they want to play again
+            Console.Write("Do you want to play again? (yes/no): ");
+            playAgain = Console.ReadLine().ToLower();
+            Console.WriteLine();
+        }
+
+        Console.WriteLine("Thanks for playing! ");
     }
 }
