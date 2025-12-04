b5c726e6ccfdba7952c84c35370b72e0c2bfca92
archibonggilbert152-hash <archibonggilbert152@gmail.com>
2025-12-04 15:37:23 -0800

    make changes to week5

---
+++ week06/Shapes/Shape.cs
@@ -0,0 +1,25 @@
+public abstract class Shape
+{
+    private string _color;
+
+    // Constructor
+    public Shape(string color)
+    {
+        _color = color;
+    }
+
+    // Getter
+    public string GetColor()
+    {
+        return _color;
+    }
+
+    // Setter
+    public void SetColor(string color)
+    {
+        _color = color;
+    }
+
+    // Virtual method for polymorphism
+    public abstract double GetArea();
+}
