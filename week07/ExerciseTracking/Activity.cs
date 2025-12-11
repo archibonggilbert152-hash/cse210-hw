e07d1e479bd8876967e960d20501c12bb337d919
archibonggilbert152-hash <archibonggilbert152@gmail.com>
2025-12-11 14:25:41 -0800

    changes to week 07

---
+++ week07/ExerciseTracking/Activity.cs
@@ -0,0 +1,34 @@
+public abstract class Activity
+{
+    private string _date;
+    private int _minutes;
+
+    public Activity(string date, int minutes)
+    {
+        _date = date;
+        _minutes = minutes;
+    }
+
+    public string GetDate()
+    {
+        return _date;
+    }
+
+    public int GetMinutes()
+    {
+        return _minutes;
+    }
+
+    // Polymorphic methods
+    public abstract double GetDistance(); // km
+    public abstract double GetSpeed();    // kph
+    public abstract double GetPace();     // min per km
+
+    public virtual string GetSummary()
+    {
+        return $"{_date} {this.GetType().Name} ({_minutes} min): " +
+               $"Distance {GetDistance():0.0} km, " +
+               $"Speed: {GetSpeed():0.0} kph, " +
+               $"Pace: {GetPace():0.00} min per km";
+    }
+}
